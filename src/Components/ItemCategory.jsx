e12e49d67bfb924c4682ebda785efb157afacfd9
Gautam Tayal <gautamtayal65@gmail.com>
2025-02-16 21:38:07 +0530

    restaurant ui with categories

--- src/Components/ItemCategory.jsx
+++ src/Components/ItemCategory.jsx
@@ -1,18 +1,22 @@
+import { useState } from "react"
 import { MenuPage } from "./MenuPage"
 
 export function ItemCategory({menuData}) {
-
+  const [clicked, setClicked] = useState(false)
 
   return(
-    <div className="menuItem flex border-b-1 p-2 justify-center content-center">
+    <div className="menuItem flex border-1 p-2 rounded-2xl justify-center content-center mb-3">
         <div className="content w-[75%]">
-        <h1 className="text-2xl font-bold">{menuData.card.card.title}</h1>
+        <h1 
+        className="text-2xl font-bold cursor-pointer"
+        onClick={() => setClicked(!clicked)}
+        >{menuData.card.card.title}</h1>
+
         <div className="menu-container">
-           <MenuPage menuData={menuData.card.card.itemCards} />
+           {clicked && <MenuPage menuData={menuData.card.card.itemCards} />}
         </div>
         
       </div>
     </div>
-    
   )
 }
